99589b44e75b70691ebeed2c547797140caab5fd
Alexander Bias <alexander.bias@uni-ulm.de>
2017-05-05 14:58:59 +0200

    Release v3.2-r3

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_people';
-$plugin->version = 2017032800;
-$plugin->release = 'v3.2-r2';
+$plugin->version = 2017050500;
+$plugin->release = 'v3.2-r3';
 $plugin->requires = 2016120500;
 $plugin->maturity = MATURITY_STABLE;
