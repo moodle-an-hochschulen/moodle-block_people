3d179ec09eed29ec9e375b90099af5ece2c79b78
Alexander Bias <bias@alexanderbias.de>
2025-10-30 21:30:44 +0100

    Release v5.1-r1

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_people';
-$plugin->version = 2025041401;
-$plugin->release = 'v5.0-r2';
+$plugin->version = 2025100600;
+$plugin->release = 'v5.1-r1';
 $plugin->requires = 2025100600;
 $plugin->supported = [501, 501];
 $plugin->maturity = MATURITY_STABLE;
